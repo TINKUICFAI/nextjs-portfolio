b03c0d1dc1ab5b617432240b7035abc8041634f0
Deepak <deepak.kumar@appsinvo.com>
2023-10-11 17:44:55 +0530

    add more projects

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -11,7 +11,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
-            href={gitUrl} target="_blank"
+            href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
@@ -27,7 +27,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className="mb-2 text-xl font-semibold">{title}</h5>
         {/* <p class="scroll-container">{description}</p> */}
-        <div className="max-h-[300px] overflow-y-auto">{description}</div>
+        <div className="max-h-[200px] overflow-y-auto">{description}</div>
       </div>
     </div>
   );
